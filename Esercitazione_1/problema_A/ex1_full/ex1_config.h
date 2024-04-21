7e1564ebe4cc1039a0fd348c23645598d03d6d56
Matteocaroleo <matteocaroleo57@gmail.com>
2024-04-21 20:15:58 +0200

    cleaned up code and refactored

---
+++ Esercitazione_1/problema_A/ex1_full/ex1_config.h
@@ -0,0 +1,52 @@
+/*  This file is used to configure the ex1_full.ino file. */
+
+#define N_OF_SAMPLES 200
+#define SAMPLING_FREQUENCY 4000
+#define FILTER_RMA
+//#define FILTER_RS
+
+#ifdef FILTER_RMA
+  #define FILTER_PTS 10
+#endif
+
+#define QUANT_4_BIT
+#define QUANT_8_BIT
+
+
+//EX_FULL configuration defines requested in assignment. It uses RMA as a low-pass filter 
+//////////////////////////////////////////////////////////////////
+//#define EX_FULL
+#ifdef EX_FULL
+  //the undef are used to reset macros to wanted values
+  #undef FILTER_RS
+  #undef N_OF_SAMPLES 
+  #undef SAMPLING_FREQUENCY
+  #undef FILTER_PTS
+
+  //EX_FULL parameters:
+  #define ANALOG_PIN A0
+  #define SAMPLING_FREQUENCY 1000 //should not be higher than 10k (arduino limit)
+
+  //WARNING: the number of samples should not be increased (only decreased)
+  #define N_OF_SAMPLES 100
+
+  //loads in program memory the filter RMA function
+  #define FILTER_RMA
+  
+  //parameter given to RMA function
+  #define FILTER_PTS 5
+#endif //EX_FULL
+////////////////////////////////////////////////////////////////////////
+
+
+//USED FOR RUNNING THE TEST ROUTINE: uncomment line below for usage
+////////////////////////////////////////////////////////////////////////// 
+#define TEST
+#ifdef TEST
+  #undef EX1_FULL
+  #undef QUANT_4_BIT 
+  #undef QUANT_8_BIT
+  #define N_OF_SAMPLES_TEST 80
+  #define FILTER_PTS_TEST 10
+#endif //TEST
+/////////////////////////////////////////////////////////////////////////
--- Esercitazione_1/problema_A/ex1_full/ex1_full.ino
+++ Esercitazione_1/problema_A/ex1_full/ex1_full.ino
@@ -1,11 +1,11 @@
-/*  MATTEO CAROLEO S5216938
+/*  MATTEO CAROLEO S5216938, done in Arduino Uno R3
     
-    This sketch manipulates input signal in pin A0. By changing values
+    This sketch manipulates input signal in analog pin. By changing values
     in the "ex1_config.h" file, it can:
       - Sample a signal with 10 bit depth
       - Sample a signal with 8 bit depth
       - Sample a signal with 4 bit depth
-      - Filter a signal with runing sum algorithm
+      - Filter a signal with running sum algorithm
       - Filter a signal with recursive moving average algorithm.
     
     If ex1_config.h is left untouched, the sketch will 
@@ -14,8 +14,10 @@
       - plot the input quantized signals with RMA filtering
     the sketch will gather N_OF_SAMPLES sample.
 
-    The extensive use of the preprocessor directives is for maximizing the waste of 
-    memory  
+    The extensive use of the preprocessor directives has been used
+    for maximizing memory usage while keeping all in a couple of files.
+
+    The sketch can also run a test, set by a macro in config.h
 */
 
 #include "ex1_config.h" 
@@ -24,23 +26,25 @@
 unsigned int sampling_period_us;
 unsigned long microseconds;
 
-//does not create array for sampling if in test mode
+//does not create array for input signal if in test mode
 #ifndef TEST
 double samples [N_OF_SAMPLES];
 #endif //TEST
 
-//creates array of different length depending if in test mode or not
+
+// creates array of different length depending if in test mode or not
 #ifdef TEST
 double filtered_samples [N_OF_SAMPLES_TEST];
 #else
 double filtered_samples [N_OF_SAMPLES];
 #endif //TEST
 
+#if defined(QUANT_8_BIT) || defined(QUANT_4_BIT)
+void Quantize ();
+#endif
 
-void Plot_signal (double* sig_src_arr, int sig_length);
-void Plot_both (double* sig_src_arr, double* sig_filtered_arr, int sig_length);
-void Plot_both_quantized_signals (double* sig_src_arr, int sig_length);
 
+  
 // used for LPF: is an estimate of an integrator
 #ifdef FILTER_RS
 void Running_sum (double* sig_src_arr, double* sig_out_arr, int sig_length);
@@ -52,11 +56,24 @@ void Recursive_moving_average(double *sig_src_arr, double* sig_out_arr ,uint32_t
 #endif //FILTER_RMA
 
 
+
+//functions for plotting data
+void Plot_signal (double* sig_src_arr, int sig_length);
+void Plot_both (double* sig_src_arr, double* sig_filtered_arr, int sig_length);
+void Plot_both_quantized_signals (double* sig_src_arr, int sig_length);
+
+// used for sampling signal in analog pin
+void Sample (double* sig_src_arr, int sig_length, unsigned int sampling_period_us, int analog_pin);
+
+// used to store quantized signals
+double temp4 [N_OF_SAMPLES];
+double temp8 [N_OF_SAMPLES];
+
 void setup() {
   Serial.begin (115200);
+  
+  //converts frequenct in microseconds
   sampling_period_us = round (1000000 * (1.0/SAMPLING_FREQUENCY));
-  
-  
 
 }
 
@@ -85,28 +102,22 @@ void loop() {
   #endif //TEST
 ////////////////////////////////////////////////////////////////////////////////////
 
-  // samples input signal with 10 bit depth and with given sampling frequency
-  for (int i = 0; i < N_OF_SAMPLES; i++){
-    microseconds = micros();   
-    samples [i] = analogRead (A0);
-    //waits for desired sampling period 
-    while (micros() < (microseconds + sampling_period_us)){}; 
-  }
-
 
 //EX_FULL: shows all request in order (using RMA filtering)
 ////////////////////////////////////////////////////////////////////////////////////
   #ifdef EX_FULL
+    // sample the input signal with 10 bit depth 
+    Sample (samples, N_OF_SAMPLES, sampling_period_us, ANALOG_PIN);
+    
     // plots signal with quantization depth of 10 bit
     Plot_signal (samples, N_OF_SAMPLES);
-    delay (800);
+    delay (2000);
 
     // plot signal with quantization depth of both 8 and 4 bit
     Plot_quantized_signals (samples, N_OF_SAMPLES);
-    delay (800);
+    delay (2000);
 
     // plot quantized and filtered signal with 8 and 4 bit of depth
-    //Serial.println ("TEST");
     Plot_quantized_and_filtered_signals (samples, N_OF_SAMPLES);
 
     //waits indefinitely
@@ -116,8 +127,9 @@ void loop() {
 
 
 // this part of code is used to run specific functionalities:
-// if EX_FULL is defined, the part below won't run
+// if EX_FULL OR TEST is defined in config, the part below won't run.
 
+#ifndef TEST
   #ifdef QUANT_4_BIT
    //maps samples from 10 bit to 4 bit
   for (int i = 0; i < N_OF_SAMPLES; i++){ 
@@ -150,14 +162,17 @@ void loop() {
   delay (500);
 
   // if the signal has been filtered, it plots the src and the filtered result
-  #if defined(FILTER_RMA) || defined (FILTER_RS)
+  #if defined(FILTER_RMA) || defined(FILTER_RS)
   Plot_both (samples, filtered_samples, N_OF_SAMPLES);
   #endif //defined(FILTER_RMA) || defined (FILTER_RS)
   
   //waits indefinitely
   while (1);
+#endif // NOT TEST
 }
 
+
+// FUNCTIONS IMPLEMENTATIONS
 
 void Plot_signal (double* sig_src_arr, int sig_length){
   for (int i = 0; i < sig_length; i++){
@@ -189,8 +204,7 @@ void Plot_quantized_signals (double* sig_src_arr, int sig_length){
 }
 
 void Plot_quantized_and_filtered_signals (double* sig_src_arr, int sig_length){
-  double temp4 [sig_length];
-  double temp8 [sig_length];
+ 
 
   for (int i = 0; i < sig_length; i++){ 
     temp8 [i] = map (sig_src_arr [i], 0, 1023, 0, 255); 
@@ -229,5 +243,17 @@ void Recursive_moving_average(double *sig_src_arr, double *sig_out_arr, uint32_t
   for(i= ceil(filter_pts/2); i<sig_length  ; i++){
       ACC =ACC + sig_src_arr[i+(filter_pts-1)/2]- sig_src_arr[i-(uint32_t)ceil(filter_pts/2)];
       sig_out_arr[i]  = ACC/filter_pts;
+  }
+}
+
+void Sample (double* sig_src_arr, int sig_length, unsigned int sampling_period_us, int analog_pin){
+  
+  unsigned long microseconds = 0;
+  // samples input signal with 10 bit depth and with given sampling frequency
+  for (int i = 0; i < sig_length; i++){
+    microseconds = micros();   
+    sig_src_arr [i] = analogRead (analog_pin);
+    //waits for desired sampling period 
+    while (micros() < (microseconds + sampling_period_us)){}; 
   }
 }
