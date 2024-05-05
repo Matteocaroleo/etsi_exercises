6a706024d1b38a48d1023235eb657f02ea272288
Matteocaroleo <matteocaroleo57@gmail.com>
2024-05-05 12:18:08 +0200

    added fft with libraries and mean square error between fft and dft

--- Esercitazione_2/problema_C/es_2_C/es_2_C.ino
+++ Esercitazione_2/problema_C/es_2_C/es_2_C.ino
@@ -1,8 +1,15 @@
 #include "ex_2_C_config.h"
+#include "arduinoFFT.h"
 
 double coefficients [5] = {0.9, 0.5, 0.7, 0.55, 0.65};
-double real_arr [N_OF_SAMPLES/2];
-double imaginary_arr [N_OF_SAMPLES/2];
+double signal [N_OF_SAMPLES] = {0};
+double real_arr [N_OF_SAMPLES];
+double imaginary_arr [N_OF_SAMPLES];
+
+//labels for the serial plotter
+char label0 [3] = {'S','I','G'};
+char label1 [3] = {'D','F','T'};
+char label2 [3] = {'F','F','T'};
 
 // The frequencies, when played together, form a C4 Maj9 chord. 
 #ifdef WAVEFORM_1
@@ -23,18 +30,71 @@ double imaginary_arr [N_OF_SAMPLES/2];
 unsigned long microseconds = 0;
 unsigned long elapsed_time = 0;
 unsigned int sampling_period_us = 0;
-double signal [N_OF_SAMPLES] = {0};
-void calc_sig_dft(double *sig_src_arr, double *sig_dest_rex_arr, double *sig_dest_imx_arr, uint32_t sig_length);
+
+void Generate_signal (double* sig_arr_out, unsigned int sampling_period_us) ;
+void Plot_signal (double* sig_src_arr, int sig_length, char* label, int lenght_of_label);
+void calc_sig_dft(double* sig_src_arr, double* sig_dest_rex_arr, double* sig_dest_imx_arr, uint32_t sig_length);
 void get_dft_output_mag (double* REX, double* IMX);
+float Mean_square_error (double* first_arr, double* second_arr, int lenght);
+//int Check_if_same (double* first_arr, double* second_arr, int lenght);
 
-void Plot_signal (double* sig_src_arr, int sig_length);
-//void Generate_signal () TO IMPLEMENT
-
-
+//SHOULD MAYBE PLOT SIGNAL ONE OVER OTHER
 void setup() {
   Serial.begin (9600);
   sampling_period_us = round(1.0e6*(1.0/SAMPLING_FREQUENCY));
+  
+  //creates the waveform
+  Generate_signal (signal, sampling_period_us);
+  Plot_signal (signal, N_OF_SAMPLES, label0, 3);
 
+  //calculates dft
+  microseconds = micros();  //starts counter
+  calc_sig_dft (signal, real_arr, imaginary_arr, N_OF_SAMPLES);
+  get_dft_output_mag (real_arr, imaginary_arr);
+  elapsed_time = micros() - microseconds; //stops counter
+  Plot_signal (real_arr, N_OF_SAMPLES/2, label1, 3);
+
+  //prints counter to serial
+  Serial.print ("ELAPSED TIME (s): ");
+  Serial.println (elapsed_time / 1.0e6);
+  delay (1000);
+  //calculates dft with libraries
+  
+  //setup: clears used arrays
+  for (int i = 0; i < N_OF_SAMPLES/2; i++){
+    imaginary_arr[i] = 0;
+    //real_arr[i] = 0;
+  }
+
+  Serial.println ("FFT WITH LIBRARY: ");
+  microseconds = micros();
+  arduinoFFT FFT = arduinoFFT();
+  FFT.Compute(signal, imaginary_arr, N_OF_SAMPLES, FFT_FORWARD); /* Compute FFT */
+  FFT.ComplexToMagnitude(signal, imaginary_arr, N_OF_SAMPLES);
+  elapsed_time = micros() - microseconds;
+  
+  Plot_signal (signal, N_OF_SAMPLES/2, label2, 3);
+  Serial.print ("ELAPSED TIME (s): ");
+  Serial.println (elapsed_time/1.0e6);
+  /*if (Check_if_same (signal, real_arr, N_OF_SAMPLES) == true){
+    Serial.println ("DFT & FFT ARE THE SAME");
+  }
+  else{
+    Serial.println ("DFT & FFT ARE NOT THE SAME");
+  }
+  */
+  //Serial.println (Check_if_same (signal, real_arr, N_OF_SAMPLES/2));
+  Serial.print ("MEAN SQUARE ERROR BETWEEN DFT & FFT: ");
+  Serial.println (Mean_square_error (signal, real_arr, N_OF_SAMPLES/2));
+
+}
+
+void loop() {
+//
+}
+
+
+void Generate_signal (double* signal, unsigned int sampling_period_us){
   for (int i = 0; i < N_OF_SAMPLES; i++){
     microseconds = micros();
     signal [i] =  coefficients [0] * sin(2.0 * PI * frequency[0] * (microseconds /1.0e6)) + 
@@ -44,43 +104,28 @@ void setup() {
                   coefficients [4] * sin(2.0 * PI * frequency[4] * (microseconds /1.0e6));
     while(micros() < (microseconds + sampling_period_us)){};
   }
-  Plot_signal (signal, N_OF_SAMPLES);
-
-  //calculates dft
-  microseconds = micros();
-  calc_sig_dft (signal, real_arr, imaginary_arr, N_OF_SAMPLES);
-  get_dft_output_mag (real_arr, imaginary_arr);
-  elapsed_time = micros() - microseconds;
-
-  Plot_signal (real_arr, N_OF_SAMPLES/2);
-  Serial.print ("ELAPSED TIME (us): ");
-  Serial.println (elapsed_time);
-
-  //calculates dft with libraries
-
+  return;
 }
 
-void loop() {
-
-}
-
-void Plot_signal (double* sig_src_arr, int sig_length){
+void Plot_signal (double* sig_src_arr, int sig_length, char* label, int lenght_of_label){
   for (int i = 0; i < sig_length; i++){
+    for (int j = 0; j < lenght_of_label; j++){
+      Serial.print (label[j]);
+    }
+    Serial.print (":");
     Serial.println (sig_src_arr [i]);
     delay (10);
   }
 }
 
-void calc_sig_dft(double *sig_src_arr, double *sig_dest_rex_arr, double *sig_dest_imx_arr, uint32_t sig_length)
-{
+void calc_sig_dft(double* sig_src_arr, double* sig_dest_rex_arr, double* sig_dest_imx_arr, uint32_t sig_length){
   int i,k,j;
   for(j=0;j<(sig_length/2);j++){
     sig_dest_rex_arr[j] =0;
     sig_dest_imx_arr[j] =0;
   }
   
-   for(k=0;k<(sig_length/2);k++)
-  {
+   for(k=0;k<(sig_length/2);k++) {
     for(i=0;i<sig_length;i++){
        sig_dest_rex_arr[k] =  sig_dest_rex_arr[k] + sig_src_arr[i]*cos(2*PI*k*i/sig_length);
        sig_dest_imx_arr[k] =  sig_dest_imx_arr[k] - sig_src_arr[i]*sin(2*PI*k*i/sig_length);
@@ -88,11 +133,34 @@ void calc_sig_dft(double *sig_src_arr, double *sig_dest_rex_arr, double *sig_des
   }
 }
 
-void get_dft_output_mag(double* REX, double* IMX)
-{
+void get_dft_output_mag(double* REX, double* IMX){
   uint32_t k;
   for(k=0;k<N_OF_SAMPLES/2;k++)
   {
     REX[k] = sqrt(pow(REX[k],2)+pow(IMX[k],2));
   }
 }
+
+float Mean_square_error (double* first_arr, double* second_arr, int lenght){
+ float acc = 0;
+ for (int i = 0; i < lenght; i ++){
+    //sum of squares
+    acc = (first_arr[i] - second_arr[i]) * (first_arr[i] - second_arr[i]);
+ } 
+ //mean
+ return acc / lenght;
+}
+
+int Check_if_same (double* first_arr, double* second_arr, int lenght){
+  //bool flag = true;
+  int flag = -1;
+  for (int i = 0; i < lenght; i++){
+    if (first_arr[i] != second_arr[i]){
+      flag= i;
+      break;
+    }
+  }
+  return flag;
+}
+
+
--- Esercitazione_2/problema_C/es_2_C/ex_2_C_config.h
+++ Esercitazione_2/problema_C/es_2_C/ex_2_C_config.h
@@ -1,26 +1,25 @@
-#define N_OF_SAMPLES 200
+#define N_OF_SAMPLES 128
 #define PI 3.1415926535897932384626433832795
 
 //#define WAVEFORM_1
-#define WAVEFORM_2
-//#define WAVEFORM_3
+//#define WAVEFORM_2
+#define WAVEFORM_3
 
 #ifdef WAVEFORM_1
   #define SAMPLING_FREQUENCY 1200 //SHOULD BE 1174, NYQUIST LIMIT
-  //#pragma message ("Running waveform 1...")
 #endif  //WAVEFORM_1
 
 #ifdef WAVEFORM_2
   #define SAMPLING_FREQUENCY 1350 //SHOULD BE 1318
-  //#pragma message ("Running waveform 2...") 
 #endif  //WAVEFORM_2
 
 #ifdef WAVEFORM_3
   #define SAMPLING_FREQUENCY 1350 //SHOULD BE 1318
-  //#pragma message ("Running waveform 3...")
 #endif  //WAVEFORM_3
 
+
 ////////////////////////___ERROR_CHECKING___////////////////////////////////
+
 #if defined (WAVEFORM_1) && defined (WAVEFORM_2) || \
     defined (WAVEFORM_1) && defined (WAVEFORM_3) || \
     defined (WAVEFORM_1) && defined (WAVEFORM_3)
@@ -30,9 +29,3 @@
 #if !defined (WAVEFORM_1) && !defined (WAVEFORM_2) && !defined (WAVEFORM_3)
   #error AT LEAST ONE WAVEFORM MUST BE DEFINED
 #endif
-
-
-
-
-
-#define SAMPLING_RATE 1/SAMPLING_FREQUENCY
